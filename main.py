2cb7d7f0ddb82daf84db2279e12aaada57f0f963
nehorayChalfon <neoray0166@gmail.com>
2025-05-20 21:50:34 +0300

    refactor feature extraction and RUL calculation in main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ df = pandas.read_csv('train_FD001.txt', sep='\s+', header=None,
                           'sensor_21'])
 
 
+### feature extraction
 # from data exloration i saw that sensors 1, 5, 6, 10, 16, 18, 19
 # are basiccly constants so have little to no effect
 # and 'sensor_11','sensor_12','sensor_8','sensor_13','sensor_9' have strong correlations and can be removed
-
 low_variance = ['sensor_1', 'sensor_5', 'sensor_6', 'sensor_10',
             'sensor_16','sensor_18','sensor_19']
 
@@ -20,11 +20,6 @@ strong_correlation = ['sensor_11','sensor_12','sensor_8','sensor_13','sensor_9',
 
 to_remove = low_variance + strong_correlation
 df_reduced = df.drop(to_remove, axis=1)
-
-print(df_reduced.head())
-
-
-
 
 
 # feature engineering
@@ -42,3 +37,12 @@ print(df_reduced.head())
 #     # Add new features to the DataFrame
 #     df[f'{sensor}_rolling_mean_{window_size}'] = rolling_mean
 #     df[f'{sensor}_rolling_std_{window_size}'] = rolling_std
+
+# Calculate the maximum cycle for each unit_id
+max_cycle_per_unit = df_reduced.groupby('unit_id')['cycle'].max()
+
+# Create the RUL column using a vectorized approach
+df_reduced['RUL'] = df_reduced['unit_id'].map(max_cycle_per_unit) - df_reduced['cycle']
+
+# Print the first few rows of df_reduced with the new RUL column
+print(df_reduced.head())
